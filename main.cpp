c85734a846215f116313bcf676732546a92e8238
王东乔 <wangdongqiao@wangdongqiaodeMacBook-Air.local>
2024-09-19 13:46:33 +0800

    test cpp17 maybe_used and change .gitingnore

--- main.cpp
+++ main.cpp
@@ -2,6 +2,11 @@
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	[[maybe_unused]] double pi { 3.14159 };  // Don't complain if pi is unused
+	[[maybe_unused]] double gravity { 9.8 }; // Don't complain if gravity is unused
+	[[maybe_unused]] double phi { 1.61803 }; // Don't complain if phi is unused
+	// The compiler will no longer warn about gravity not being used
+	//But I don't know why this is also a warning :not used
+	//In Clion for Mac ,APPLE-CLANG 15,clang-tidy(default)
 	return 0;
 }
